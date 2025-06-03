70c31964c6a700494a3eac6b550f00a08bd6b868
Iulya10 <toader10iulia@gmail.com>
2025-06-03 09:27:55 +0300

    Create url_analyzer.py

---
+++ url_analyzer.py
@@ -0,0 +1,15 @@
+# Importăm librăria re pentru expresii regulate (regex)
+import re
+
+# Importăm urlparse ca să descompunem URL-ul
+from urllib.parse import urlparse
+
+# Importăm socket și ssl pentru verificare DNS și SSL
+import socket
+import ssl
+
+# Importăm whois ca să aflăm cine a înregistrat domeniul și când
+import whois
+
+# Importăm datetime pentru a verifica dacă domeniul e recent
+from datetime import datetime
